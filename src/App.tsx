f0e4ed03a395af29fb313c992e61c167e1abc442
Anddy Luciano <luciano.ratsimbazafindramanana@gmail.com>
2023-10-13 13:21:24 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import { useEffect, useMemo, useState } from "react";
 import "./App.css";
 
 function App() {
+    const [t, setT] = useState<number>(0);
+
+    const [isRunning, setIsRunning] = useState<boolean>(false);
+
+    useEffect(() => {
+        let intervalId: number;
+        if (isRunning) {
+            intervalId = setInterval(() => {
+                setT(t + 1);
+                console.log(t);
+            }, 10);
+        }
+        return () => clearInterval(intervalId);
+    }, [isRunning, t]);
+
+    const seconde = useMemo(() => {}, [t]);
+
     return (
         <>
+            <button onClick={() => setIsRunning(!isRunning)}>test</button>
             <span className="text-red-500 underline">Hello world</span>
+            <span>
+                {t.toString().length < 2 ? t.toString().padStart(2, "0") : t}
+            </span>
         </>
     );
 }
