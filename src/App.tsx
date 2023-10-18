be1652be8107bd556b99e15f2fa9543fe0bb3d28
Anddy Luciano <luciano.ratsimbazafindramanana@gmail.com>
2023-10-18 13:13:05 +0000

    finished stopwatch

--- src/App.tsx
+++ src/App.tsx
@@ -43,57 +43,49 @@ function App() {
         setHour(0);
         setOpenDialog(true);
     };
-
-    const closeDialog = () => {
-        setOpenDialog(false);
-    };
-
-    console.log(openDialog);
-
     return (
-        <main className="relative">
-            <div>
-                <span>
-                    {hour.toString().length < 2
-                        ? hour.toString().padStart(2, "0")
-                        : hour}
-                    :
-                    {minute.toString().length < 2
-                        ? minute.toString().padStart(2, "0")
-                        : minute}
-                    :
-                    {seconde.toString().length < 2
-                        ? seconde.toString().padStart(2, "0")
-                        : seconde}
-                </span>
-            </div>
-            <div className="flex gap-2">
-                <button onClick={startOrPauseTimer}>
-                    {!isRunning ? "START" : "PAUSE"}
-                </button>
-                <button
-                    onClick={stopTimer}
-                    disabled={openDialog === true || t <= 0}
-                >
-                    STOP
-                </button>
-            </div>
-            <dialog
-                open={openDialog === true}
-                className="absolute top-0 left-0 w-screen h-screen backdrop-blur-[1px] bg-gray-700/10"
-            >
-                <div className="w-full h-full flex items-center justify-center">
-                    <div className="bg-white p-4 rounded-md relative shadow-lg">
-                        <p>ceci n'est qu'un test</p>
-                        <button
-                            className="absolute -top-2 -right-1"
-                            onClick={closeDialog}
+        <main className="relative w-screen h-screen bg-slate-100">
+            <div className="w-full h-full flex items-center justify-center flex-col">
+                <div className="bg-white w-1/5 h-1/4 rounded-sm shadow-[10px_10px_4px_-4px_rgba(213,220,217,1)]">
+                    <div className="text-5xl font-open-sans text-[#657070] h-3/4 flex items-center justify-center relative">
+                        <span>
+                            {hour.toString().length < 2
+                                ? hour.toString().padStart(2, "0")
+                                : hour}
+                            :
+                            {minute.toString().length < 2
+                                ? minute.toString().padStart(2, "0")
+                                : minute}
+                            :
+                            {seconde.toString().length < 2
+                                ? seconde.toString().padStart(2, "0")
+                                : seconde}
+                        </span>
+                        <div className="w-20 text-center absolute top-2 right-2 rounded-sm bg-blue-500 text-white text-xs px-2 py-1 ">
+                            {isRunning ? "RUNNING" : "STOPPED"}
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-2 h-1/4">
+                        <div
+                            onClick={startOrPauseTimer}
+                            className="bg-cyan-700 text-white flex items-center justify-center hover:cursor-pointer"
                         >
-                            <i className="fa-solid fa-circle-xmark text-red-500"></i>
+                            {!isRunning ? (
+                                <i className="fa-solid fa-play"></i>
+                            ) : (
+                                <i className="fa-solid fa-pause"></i>
+                            )}
+                        </div>
+                        <button
+                            onClick={stopTimer}
+                            disabled={openDialog === true || t <= 0}
+                            className="bg-blue-300 text-white"
+                        >
+                            <i className="fa-solid fa-stop"></i>
                         </button>
                     </div>
                 </div>
-            </dialog>
+            </div>
         </main>
     );
 }
